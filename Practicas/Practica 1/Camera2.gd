9db21f04f4ac1e21433c3fc2886c87001fb1d7d5
diego <diegohm1598@gmail.com>
2022-02-25 14:26:29 +0100

    PRactica 1 casi terminada

--- Practicas/Practica 1/Camera2.gd
+++ Practicas/Practica 1/Camera2.gd
@@ -1,10 +1,9 @@
 extends Camera
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var seconds = 0
+var SENSIBILITY = 0.01
+var LIMIT_Y_UP = 0.8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +12,45 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation_degrees.y += 0.5
+	if Input.is_action_pressed("Left Camera"):
+		print("y:")
+		print(get_camera_transform().basis.y.y)
+		print("\n")
+		print("z:")
+		print(get_camera_transform().basis.y.x)
+		print("\n")
+		rotate_y(SENSIBILITY)
+	if Input.is_action_pressed("Right Camera"):
+		print("y:")
+		print(get_camera_transform().basis.y.y)
+		print("\n")
+		print("z:")
+		print(get_camera_transform().basis.y.x)
+		print("\n")
+		rotate_y(-SENSIBILITY)
+	if Input.is_action_pressed("Up Camera") and get_camera_transform().basis.y.y >= LIMIT_Y_UP and get_camera_transform().basis.y.z < 0.5:
+		print("y:")
+		print(get_camera_transform().basis.y.y)
+		print("\n")
+		print("z:")
+		print(get_camera_transform().basis.y.x)
+		print("\n")
+		rotate_object_local(Vector3(1, 0, 0), SENSIBILITY)
+	if Input.is_action_pressed("Down Camera"):# and get_camera_transform().basis.y.y >= LIMIT_Y_UP and get_camera_transform().basis.y.z > -0.5:
+		print("y:")
+		print(get_camera_transform().basis.y.y)
+		print("\n")
+		print("z:")
+		print(get_camera_transform().basis.y.x)
+		print("\n")
+		rotate_object_local(Vector3(1, 0, 0), -SENSIBILITY)
+
+func _orbit_y(s):
+	rotate_y(s)
+
+func _orbit_every_second(delta):
+	seconds += delta
+
+	if seconds >= 1:
+		_orbit_y(1)
+		seconds = 0
